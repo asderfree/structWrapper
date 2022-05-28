11d2cd9d159699ac4dbd8c940e7330ff5ba852f3
asderfree <3302860184@qq.com>
2022-05-28 22:55:19 +0800

    20220528

---
+++ designpattern/structurepattern/flyweight/flyweight.go
@@ -0,0 +1,7 @@
+package structurePattern
+
+/**
+ *The flyweight pattern is a struct pattern which will share many object's state to load as much things in the same memory
+ * 享元模式不建议在对象中存储对象的外在状态，而是将其传递给依赖于它的一个特殊方法，
+ * 程序中之保持对象的内在状态。
+ */
