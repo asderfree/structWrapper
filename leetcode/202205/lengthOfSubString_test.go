a37bb416d77248747c4863ed122e0a856c0f3ca6
asderfree <3302860184@qq.com>
2022-05-29 18:43:57 +0800

    longest sub string

---
+++ leetcode/202205/lengthOfSubString_test.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import "testing"
+
+func Test_lengthOfLongestSubstring(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		// {"test1", args{"abcdaacewofhweo"}, 4},
+		{"test2", args{"abcabcbb"}, 3},
+		{"test3", args{"bbbbbbbbb"}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLongestSubstring(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
