f7eb2555df23610718e4c558061e42f9cfc0cd46
asderfree <3302860184@qq.com>
2022-06-03 00:10:08 +0800

    c++ factory

--- designpattern/createPattern/factory/cpp/factory.cpp
+++ designpattern/createPattern/factory/cpp/factory.cpp
@@ -1,1 +1,36 @@
 #include "factory.h"
+
+std::string iGun::getName()
+{
+    return this->name;
+}
+
+int iGun::getPower()
+{
+    return this->power;
+}
+
+void iGun::setName(std::string name) 
+{
+    this->name = name;
+}
+
+void iGun::setPower(int power)
+{
+    this->power = power;
+}
+
+void iGun::show()
+{
+    printf("The Name is: %s and the power is: %d\n", this->name.c_str(), this->power);
+}
+
+iGun* AKFactory::createGun(std::string name, int power)
+{
+    return new ak47(name, power);
+}
+
+iGun* UZIFactory::createGun(std::string name, int power)
+{
+    return new uzi(name, power);
+}
--- designpattern/createPattern/factory/cpp/factory.h
+++ designpattern/createPattern/factory/cpp/factory.h
@@ -1,9 +1,12 @@
+#ifndef FACTORY_H
+#define FACTORY_H
+
 #include <iostream>
 #include <stdio.h>
 
 class iGun
 {
-    private:
+    public:
     std::string name;
     int power;
     public:
@@ -13,3 +16,47 @@ class iGun
     virtual void setPower(int power);
     virtual void show();
 };
+
+
+class ak47: public iGun
+{
+    public:
+    ak47(std::string name, int power){
+        this->name = name;
+        this->power = power;
+    }
+    ~ak47(){
+    }
+};
+
+class uzi: public iGun
+{
+    public:
+    uzi(std::string name, int power)
+    {
+        this->name = name;
+        this->power = power;
+    }
+    ~uzi(){
+    }
+};
+
+class iFactory
+{
+    public:
+    virtual iGun* createGun(std::string name, int power) = 0;
+};
+
+class AKFactory: public iFactory
+{
+    public:
+    iGun* createGun(std::string name, int power);
+};
+
+class UZIFactory: public iFactory
+{
+    public:
+    iGun* createGun(std::string name, int power);
+};
+
+#endif
