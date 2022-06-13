c39373f65594de082a8f7f8caf0dc37f2b2e3584
asderfree <3302860184@qq.com>
2022-06-13 23:07:19 +0800

    modify

--- leetcode/202206/Triangle.go
+++ leetcode/202206/Triangle.go
@@ -6,14 +6,25 @@ package leetcode
  */
 
 func minimumTotal(triangle [][]int) int {
+	if len(triangle) == 0 {
+		return triangle[0][0]
+	}
 	ret := (1 << 31)
 	for i := 1; i < len(triangle); i++ {
 		for j := 0; j <= i; j++ {
-			t := triangle[i][j]
-			triangle[i][j] = t + triangle[i-1][j]
-			if j < i && triangle[i][j] > t+triangle[i-1][j] {
-				triangle[i][j] = t + triangle[i-1][j]
+			t := 0
+			if j == 0 {
+				t = triangle[i-1][j]
+			} else if j == i {
+				t = triangle[i-1][j-1]
+			} else {
+				if triangle[i-1][j] < triangle[i-1][j-1] {
+					t = triangle[i-1][j]
+				} else {
+					t = triangle[i-1][j-1]
+				}
 			}
+			triangle[i][j] += t
 			if i == len(triangle)-1 && ret > triangle[i][j] {
 				ret = triangle[i][j]
 			}
