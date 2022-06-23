bdf581cef4eeb08b321798febccf02e23f3bfb45
hawkingdong <hawkingdong@futunn.com>
2022-06-23 13:21:25 +0800

    1

---
+++ leetcode/202206/Further_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "testing"
+
+func Test_furthestBuilding(t *testing.T) {
+	type args struct {
+		heights []int
+		bricks  int
+		ladders int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"test1", args{heights: []int{4, 12, 2, 7, 3, 18, 20, 3, 19}, bricks: 10, ladders: 3}, 8},
+		{"test1", args{heights: []int{4, 2, 7, 6, 9, 14, 12}, bricks: 5, ladders: 1}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := furthestBuilding(tt.args.heights, tt.args.bricks, tt.args.ladders); got != tt.want {
+				t.Errorf("furthestBuilding() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
